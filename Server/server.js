d8bdd9c637a27e0c5f823a6fcec0e598fdf07890
apurw <apurwachandrakar156@gmail.com>
2026-03-29 12:41:47 +0530

    new fix

--- Server/server.js
+++ Server/server.js
@@ -2,21 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
-import { inngest, functions } from "./inngest/index.js";
+import {inngest,functions} from './inngest/index.js' 
 import { serve } from "inngest/express";
-
 const app = express();
-
-connectDB();
-
+await connectDB();
 app.use(express.json());
 app.use(cors());
-
 app.get("/", (req, res) => res.send("server is running"));
+app.use('api/inngest',serve({client:inngest,functions}))
+const PORT = process.env.PORT || 3333;
 
-// ✅ Inngest route
-app.use("/api/inngest", serve({ client: inngest, functions }));
-
-// ❌ REMOVE app.listen
-// ✅ EXPORT app
-export default app;
+app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`));
