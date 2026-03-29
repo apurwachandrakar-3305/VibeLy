afb32049c07dc377371dfa6c09589638aef57260
apurw <apurwachandrakar156@gmail.com>
2026-03-29 11:47:56 +0530

    commit

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ await connectDB();
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => res.send("server is running"));
-app.use('/api/inngest',serve({client:inngest,functions}))
+app.use('/inngest',serve({client:inngest,functions}))
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`));
