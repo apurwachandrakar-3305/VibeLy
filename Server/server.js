55074c4a1378118d019eb8b2c4a7bc9234f258ea
apurw <apurwachandrakar156@gmail.com>
2026-03-29 11:51:12 +0530

    inngestFix

--- Server/server.js
+++ Server/server.js
@@ -2,14 +2,21 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
-import {inngest,functions} from './inngest/index.js' 
+import { inngest, functions } from "./inngest/index.js";
 import { serve } from "inngest/express";
+
 const app = express();
+
 await connectDB();
+
 app.use(express.json());
 app.use(cors());
-app.get("/", (req, res) => res.send("server is running"));
-app.use('/inngest',serve({client:inngest,functions}))
-const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => console.log(`Server is Running on port ${PORT}`));
+app.get("/", (req, res) => res.send("server is running"));
+
+// ✅ Inngest route
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+// ❌ REMOVE app.listen
+// ✅ EXPORT app
+export default app;
