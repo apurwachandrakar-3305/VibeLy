2e3dcf4cf199224d892b0ded91866d206aa4320e
apurw <apurwachandrakar156@gmail.com>
2026-04-07 12:29:40 +0530

    mongoDbMistake

--- Server/models/User.js
+++ Server/models/User.js
@@ -31,9 +31,9 @@ const userSchema = new mongoose.Schema(
     },
     cover_photo: { type: String, default: "" },
     location: { type: String, default: "" },
-    followers: { type: [String], ref: "User", default: [] },
-    following: { type: [String], ref: "User", default: [] },
-    connections: { type: [String], ref: "User", default: [] },
+    followers: { type: [String], ref: "User" },
+    following: { type: [String], ref: "User" },
+    connections: { type: [String], ref: "User" },
   },
   { timestamps: true, minimize: false },
 );
